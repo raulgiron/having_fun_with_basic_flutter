2c6ea167479c21920a8595fd81f2d46013e617ed
Raul Alejandro Giron Jimenez <raulgiron@hotmail.com>
2023-01-22 18:52:11 -0400

    Hello World App

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+    child: Text(
+      'Hola Mundo!',
+      style: TextStyle(
+        height: 0,
+        fontSize: 40,
+      ),
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
