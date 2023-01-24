f03fda88420ed6aa6883a779beaa51ed137ee88b
Raul Alejandro Giron Jimenez <raulgiron@hotmail.com>
2023-01-24 17:05:00 -0400

    column and statelesswidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,57 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import "package:flutter/foundation.dart";
+import "package:flutter/material.dart";
 
 void main() {
-  runApp(MaterialApp(
-    title: 'MyApp',
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Title App'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  List<String> notes = [
+    "Raul's Learning Flutter",
+    "Make breakfast",
+    "Take a shower",
+    "Go to work",
+    "Take a lunch",
+    "Get back to work",
+    "Go Home to rest",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MyApp",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Title App"),
+        ),
+        body: Column(
+          children: notes.map((e) => Reminders(e)).toList(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (() {
+            if (kDebugMode) {
+              print("Pressed Click Button");
+            }
+          }),
+          child: const Icon(Icons.view_list),
+        ),
       ),
-      body: const Center(
-        child: Text('Hola Mundo!', style: TextStyle(height: -1, fontSize: 40)),
+    );
+  }
+}
+
+class Reminders extends StatelessWidget {
+  Reminders(this.note, {super.key});
+  String note;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Center(
+        child: Text(note),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (() {
-          if (kDebugMode) {
-            print('Click');
-          }
-        }),
-        child: const Icon(Icons.view_list),
-      ),
-    ),
-  ));
+    );
+  }
 }
