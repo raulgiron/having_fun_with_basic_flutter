a873f44a6b6b5e0f820e90773d387525408a22c0
Raul Alejandro Giron Jimenez <raulgiron@hotmail.com>
2023-01-24 21:01:11 -0400

    stateful widget, state widget and inistate widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,43 @@ import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  List<String> notes = [
-    "Raul's Learning Flutter",
-    "Make breakfast",
-    "Take a shower",
-    "Go to work",
-    "Take a lunch",
-    "Get back to work",
-    "Go Home to rest",
-  ];
+  @override
+  State<StatefulWidget> createState() {
+    return StateMyApp();
+  }
+}
+
+class StateMyApp extends State<StatefulWidget> {
+  late List<String> notes;
+
+  @override
+  void initState() {
+    super.initState();
+    if (kDebugMode) {
+      print("Init State Method Called");
+    }
+    notes = [
+      "Raul's Learning Flutter",
+      "Make breakfast",
+      "Take a shower",
+      "Go to work",
+      "Take a lunch",
+      "Get back to work",
+      "Go Home to rest",
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (kDebugMode) {
+      print("Method build Refreshed");
+    }
     return MaterialApp(
       title: "MyApp",
       home: Scaffold(
@@ -31,9 +50,13 @@ class MyApp extends StatelessWidget {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: (() {
-            if (kDebugMode) {
-              print("Pressed Click Button");
-            }
+            setState(() {
+              if (kDebugMode) {
+                notes.add("New note added");
+                print("Pressed Click Button");
+                print(notes.length);
+              }
+            });
           }),
           child: const Icon(Icons.view_list),
         ),
